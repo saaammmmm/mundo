942ad78f3cad86fe9b11025ff39d7583116d8447
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-17 14:30:27 -0700

    Setting up db stuff

--- ObeRift/ObeRift.js
+++ ObeRift/ObeRift.js
@@ -3,33 +3,24 @@
 
 
 
-
-if (Meteor.isClient) {/*
-  Template.home.autoredirect = function(){
+if (Meteor.isClient) {
+  /*Template.home.autoredirect = function(){
     Router.go('join');
-  }
-*/
-   console.log("Welcome, client to OBERift");
-  /*Router.map( function () {
-    // simple route with
-    // name 'home' that
-    // matches '/' and automatically renders
-    // template 'home'
-    this.route('home', {path: '/'});
-  }); // end of Router.map()...
+  }*/
 
-*/
-
-
-
-
-  /* // Discern what the hell we are doing
+  // Discern what the hell we are doing
   console.log("Creating users table")
   UserTable = new Meteor.Collection('Users'); // Do not user var
   console.log("Users table created");
   console.log("Welcome, client to OBERift")
 
+  
+Router.map(function(){
+  this.route('home', {path: '/'});
+  this.route('join', {path: '/join'});
+}); // end of Router.map()...
 
+/*
     UserTable.insert({
         name: "Sam",
         rank: 9000
@@ -37,14 +28,13 @@ if (Meteor.isClient) {/*
     }); // End if insert...
     
   UserTable.find().fetch();
-*/
+
 }
 
 if (Meteor.isServer) {
-  /*
-    console.log("Welcome, server to OBERift");
+    console.log("Welcome, server to OBERift")
 
     UserTable = new Meteor.Collection('Users'); // dp not use var
-*/
+
 
 }
