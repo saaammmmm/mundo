cf161546039c87e044c9f77698715c40ff0591ef
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-17 13:34:01 -0700

    Setting up db stuff

--- ObeRift/ObeRift.js
+++ ObeRift/ObeRift.js
@@ -1,18 +1,18 @@
 // Create the table
   
 
-  Router.map(function(){
-    this.route('home', {path: '/'});
-    //this.route('join', {path: '/join'});
-  }); // end of Router.map()...
 
 
-/*
+
 if (Meteor.isClient) {
   Template.home.autoredirect = function(){
     Router.go('join');
   }
 
+  Router.map(function(){
+    this.route('home', {path: '/'});
+    //this.route('join', {path: '/join'});
+  }); // end of Router.map()...
   // Discern what the hell we are doing
   console.log("Creating users table")
   UserTable = new Meteor.Collection('Users'); // Do not user var
@@ -37,4 +37,3 @@ if (Meteor.isServer) {
 
 
 }
-*/
