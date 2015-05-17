4d5db993ddcc20c206622e9f631ed788961ec006
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-17 13:25:34 -0700

    Setting up db stuff

--- ObeRift/ObeRift.js
+++ ObeRift/ObeRift.js
@@ -11,24 +11,22 @@ Router.map(function(){
 if (Meteor.isClient) {
   /*Template.home.autoredirect = function(){
     Router.go('join');
-  }*/
+  }
   // Discern what the hell we are doing
   console.log("Creating users table")
   UserTable = new Meteor.Collection('Users'); // Do not user var
   console.log("Users table created");
   console.log("Welcome, client to OBERift")
 
-  
-function insertSomeData(){
-    // Insert some dummy data
+/*
     UserTable.insert({
         name: "Sam",
         rank: 9000
 
     }); // End if insert...
-    }
+    
   UserTable.find().fetch();
-
+*/
 }
 
 if (Meteor.isServer) {
