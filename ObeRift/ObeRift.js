2b5b7b1fbb37389c721240b4ce5a6f50009d9d1b
cheng1994 <gordon.chen94@gmail.com>
2015-05-17 20:56:03 -0700

    Adding api calls

--- ObeRift/ObeRift.js
+++ ObeRift/ObeRift.js
@@ -7,20 +7,25 @@ if (Meteor.isClient) {
 
   userStats = new Meteor.collection('userStats');
 
+  Meteor.call(getLoLAccount("Tiandi"));
+
 
 }
 if (Meteor.isServer) {
     console.log("Welcome, server to OBERift");
 
-    Meteor.http.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Tiandi?api_key=d1269d52-93a3-48b8-a4c9-1961975da3b5", function(error, result){
-        if(error){
-            console.log("Error");
-        }
-        else {
-            console.log("http get SUCCESS");
-            if(result.statusCode === 200) {
-                console.log('Status code = 200!');
-                console.log(result.content);
+    Meteor.methods({
+        getLoLAccount: function(userName){
+            var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + userName + "?api_key=d1269d52-93a3-48b8-a4c9-1961975da3b5";
+            var result = Meteor.http.get(url, {timeout: 30000});
+            if(result.statusCode == 200){
+                var response = JSON.parse(result.content);
+                console.log(response);
+                return response;
+            } else {
+                console.log("Response issue: ", result.statusCode);
+                var errorJson = JSON.parse(result.content);
+                throw new Meteor.Error(result.statusCode, errorJson.error);
             }
         }
     });
