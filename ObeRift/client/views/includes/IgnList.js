5846360f191ddae4313485d99b15c7a8f0239a38
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-31 16:09:34 -0700

    Made it so taht the dashboard displays: Game, IGN (level as a badge) for taht game. We need to expand it beyond leauge of legends and find a way to maek it so you can only add a game to your dashboard taht you actually are that person. I.E. i can add you or my friend onto my page right now. Will this be done upon game createion through the API?

--- ObeRift/client/views/includes/IgnList.js
+++ ObeRift/client/views/includes/IgnList.js
@@ -1,5 +1,11 @@
 Template.IgnList.helpers({
     IgnList: function() {
         return IgnList.find({obeUserName: Meteor.user().username});
+    },
+    level: function(inGameName) {
+        
+        var gameName =  inGameName.hash.IGN;
+        console.log(gameName);
+        return GameList.findOne({IGN: gameName}).level;
     }
 });
