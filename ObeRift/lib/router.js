815058855d33c63f44b84b4ce071a7ed4b788764
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-31 12:10:52 -0700

    Get started works now. There is a place to log in or create accounts. Made a new template called join

--- ObeRift/lib/router.js
+++ ObeRift/lib/router.js
@@ -2,5 +2,5 @@ Router.map(function(){
     this.route('home',          {path: '/'});
     this.route('dashboard',     {path: 'dashboard'});
     this.route('teams',         {path: 'teams'});
-
+    this.route('join',          {path: 'join'});
 }); // end of Router.map()...
