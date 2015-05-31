84892b865d31742a836fdd57fc5b2a652a90df1d
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-30 19:22:57 -0700

    Added page to signup

--- ObeRift/lib/router.js
+++ ObeRift/lib/router.js
@@ -2,4 +2,6 @@ Router.map(function(){
     this.route('home',          {path: '/'});
     this.route('dashboard',     {path: 'dashboard'});
     this.route('teams',         {path: 'teams'});
+    this.route('registration',  {path: 'registration'});
+
 }); // end of Router.map()...
