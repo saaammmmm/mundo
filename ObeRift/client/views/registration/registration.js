49c6349afd07940c4be93bc8632379549e77f6fa
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-05-31 00:35:43 -0700

    Made it so that you can click the getting started to show the accounts ui popup. Not the best. Need to find a way to take the usre to a form and have the acounts ui create acount fields shown in the form

--- ObeRift/client/views/registration/registration.js
+++
@@ -1,3 +0,0 @@
-Template.registration.helpers({
-
-});
--- ObeRift/lib/router.js
+++ ObeRift/lib/router.js
@@ -2,6 +2,5 @@ Router.map(function(){
     this.route('home',          {path: '/'});
     this.route('dashboard',     {path: 'dashboard'});
     this.route('teams',         {path: 'teams'});
-    this.route('registration',  {path: 'registration'});
 
 }); // end of Router.map()...
