efaf82596ca25a583e993247f0aad668ac81a032
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-06-07 18:16:26 -0700

    Adding Typahead

--- ObeRift/client/views/dashboard/dashboard.js
+++ ObeRift/client/views/dashboard/dashboard.js
@@ -1,3 +1,15 @@
+// (c) 2015 Samuel Jacobs, Gorden Chen
+
+// Using iron:router the Meteor.startup is already triggered because it loads the 
+// template or the loading template and then inject the data. It must be delayed 
+// to when iron:router knows it is rendered completely.
+
+
+Template.dashboard.rendered = function() {
+    console.log("Injecting typeahead");
+      Meteor.typeahead.inject();
+};
+
 Template.dashboard.events({
     "submit .form-inline": function(event){
         event.preventDefault();
@@ -17,8 +29,18 @@ Template.dashboard.events({
     }
 });
 
-Template.dashboard.helpers({
-    stats: function() {
-        return GameList.find();
+
+    Template.dashboard.helpers({
+        // Populates the stats table
+        stats: function() {
+            return GameList.find();
+        },
+        // used to add new games to the stats field and in autocomplete searches
+        supportedGames: function() {
+            return SupportedGames.find().fetch().map(function(it){ return it.name; });
+        }
+    });
+
+    Template.dashboard.rendered = function() {
+        Meteor.typeahead();
     }
-});
