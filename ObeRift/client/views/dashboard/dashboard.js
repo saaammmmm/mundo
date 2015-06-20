154637c813ce41ba14220c750afdc22d3b0318df
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-06-20 00:50:49 -0700

    working on teams

--- ObeRift/client/views/dashboard/dashboard.js
+++ ObeRift/client/views/dashboard/dashboard.js
@@ -10,37 +10,20 @@ Template.dashboard.rendered = function() {
       Meteor.typeahead.inject();
 };
 
-Template.dashboard.events({
-    "submit .form-inline": function(event){
-        event.preventDefault();
-        var obeUserName = Meteor.user().username;
-        var game = event.target.gameTitle.value;
-        var playerName = event.target.gameUserName.value;
 
-       /* console.log("Inserting:\n" + obeUserName + "\n" + game + "\n" + playerName);*/
-        IgnList.insert({
-            obeUserName: obeUserName,
-            name: playerName,
-            game: game
-        });
-        event.target.gameTitle.value = "";
-        event.target.gameUserName.value = "";
-        Meteor.call('getLoLAccount', playerName);
+
+
+Template.dashboard.helpers({
+    // Populates the stats table
+    stats: function() {
+        return GameList.find();
+    },
+    // used to add new games to the stats field and in autocomplete searches
+    supportedGames: function() {
+        return SupportedGames.find().fetch().map(function(it){ return it.name; });
     }
 });
 
-
-    Template.dashboard.helpers({
-        // Populates the stats table
-        stats: function() {
-            return GameList.find();
-        },
-        // used to add new games to the stats field and in autocomplete searches
-        supportedGames: function() {
-            return SupportedGames.find().fetch().map(function(it){ return it.name; });
-        }
-    });
-
-    Template.dashboard.rendered = function() {
-        Meteor.typeahead();
-    }
+Template.dashboard.rendered = function() {
+    Meteor.typeahead();
+}
--- ObeRift/client/views/teams/teams.js
+++ ObeRift/client/views/teams/teams.js
@@ -3,3 +3,29 @@ Template.teams.helpers({
         return Teams.find();
     }
 });
+
+Template.dashboard.events({
+    // PUT THIS ON THE SERVER SIDE!!!
+    "submit createTeamButton": function(event){
+        console.log("Creating new team")
+        event.preventDefault();
+        var obeUserName = Meteor.user().username;
+        var name = $('#teamName').val();
+        var game = $('#teamGame').val();
+        var battleCry = $('#teamBattlecry').val();
+        var teamCaptain = $('#teamCaptain').val();
+
+       /* console.log("Inserting:\n" + obeUserName + "\n" + game + "\n" + playerName);*/
+        Teams.insert({
+            obeUserName: obeUserName,
+            name: name,
+            game: game,
+            teamCaptain: game,
+            battleCry: battleCry
+        });
+        event.target.name.value = "";
+        event.target.game.value = "";
+        event.target.teamCaptain.value = "";
+        event.target.battleCry.value = "";
+    }
+});
